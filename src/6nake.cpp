ecd214f68c850d147e1a8403ea7e3de1d01bfb30
Kirit Sælensminde <kirit@kirit.com>
2022-10-21 19:06:00 +0700

    Create a window

--- src/6nake.cpp
+++ src/6nake.cpp
@@ -1,2 +1,24 @@
+#include <SDL.h>
 
-int main() { return 0; }
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *const window = SDL_CreateWindow(
+            "6nake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
+            0);
+
+    for (bool quit = false; not quit;) {
+        SDL_Event event;
+        SDL_WaitEvent(&event);
+
+        switch (event.type) {
+        case SDL_QUIT: quit = true; break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
