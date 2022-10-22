203ec7e72017beb621896282f709c81581cd9155
Kirit Sælensminde <kirit@felspar.com>
2022-10-22 12:20:46 +0700

    Switch to using planet-sdl as a wrapper

--- src/6nake.cpp
+++ src/6nake.cpp
@@ -1,24 +1,33 @@
-#include <SDL.h>
+#include <planet/sdl.hpp>
 
 
 int main() {
-    SDL_Init(SDL_INIT_VIDEO);
+    planet::sdl::init sdl;
 
     SDL_Window *const window = SDL_CreateWindow(
             "6nake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
             0);
+    SDL_Renderer *const renderer = SDL_CreateRenderer(window, -1, 0);
+
 
     for (bool quit = false; not quit;) {
+        SDL_Delay(10);
+
         SDL_Event event;
         SDL_WaitEvent(&event);
 
         switch (event.type) {
         case SDL_QUIT: quit = true; break;
         }
+
+        SDL_SetRenderDrawColor(renderer, 5, 5, 5, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    SDL_Quit();
 
     return 0;
 }
