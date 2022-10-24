77d485a50c80e9db5c470475207a552ed6c4e688
Kirit Sælensminde <kirit@felspar.com>
2022-10-24 15:44:58 +0700

    Draw map features

--- src/draw.cpp
+++ src/draw.cpp
@@ -2,6 +2,23 @@
 #include <6nake/player.hpp>
 
 
+namespace {
+    void draw_hex(
+            planet::sdl::drawframe &draw,
+            planet::hexmap::coordinates const loc,
+            float const radius) {
+        auto const vertices = loc.vertices(1.0f, radius);
+        std::array<SDL_Point, 7> drawing{};
+        for (std::size_t index{}; auto v : vertices) {
+            auto const p = draw.viewport.into(v);
+            drawing[index++] = {int(p.x()), int(p.y())};
+        }
+        drawing[6] = drawing[0];
+        draw.lines(drawing);
+    }
+}
+
+
 void draw::world(
         planet::sdl::drawframe &draw,
         mapgen::hex::world_type const &world,
@@ -12,15 +29,39 @@ void draw::world(
     auto const bottom_right = player.position
             + planet::hexmap::coordinates{range + 1, -range - 1};
 
-    draw.colour(200, 200, 200);
     for (auto const loc :
          planet::hexmap::coordinates::by_column(top_left, bottom_right)) {
-        auto const vertices = loc.vertices(1.0f, 0.9f);
-        for (std::size_t index{}; index < vertices.size(); ++index) {
-            auto const next = (index + 1) % vertices.size();
-            auto const v0 = draw.viewport.into(vertices[index]);
-            auto const v1 = draw.viewport.into(vertices[next]);
-            draw.line(v0.x(), v0.y(), v1.x(), v1.y());
+        auto const &cell = world[loc];
+        if ((player.position - loc).mag2() <= range * range) {
+            if (player.position == loc or cell.player) {
+                draw.colour(0, 255, 0);
+                draw_hex(draw, loc, 0.8f);
+            } else {
+                switch (cell.features) {
+                case mapgen::feature::none:
+                    draw.colour(120, 120, 120);
+                    draw_hex(draw, loc, 0.9f);
+                    break;
+                case mapgen::feature::rock:
+                    draw.colour(255, 255, 255);
+                    draw_hex(draw, loc, 0.9f);
+                    draw_hex(draw, loc, 0.7f);
+                    draw_hex(draw, loc, 0.5f);
+                    break;
+                case mapgen::feature::food:
+                    draw.colour(0, 0, 255);
+                    draw_hex(draw, loc, 0.3f);
+                    break;
+                case mapgen::feature::food_plus:
+                    draw.colour(120, 120, 255);
+                    draw_hex(draw, loc, 0.5f);
+                    break;
+                case mapgen::feature::vision_plus:
+                    draw.colour(255, 120, 255);
+                    draw_hex(draw, loc, 0.4f);
+                    break;
+                }
+            }
         }
     }
 }
