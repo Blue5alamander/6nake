f413a9ccf1e84f27ccb1e70091982320e493a672
Kirit Sælensminde <kirit@felspar.com>
2022-10-27 18:37:14 +0700

    Animate scaling

--- src/6nake.cpp
+++ src/6nake.cpp
@@ -21,44 +21,61 @@ int main() {
     auto world = mapgen::create_map();
     player::snake player{world};
 
+    /// Auto scaling with scaling animation
+    auto const auto_scale = [&window, &player]() {
+        return std::min(window.width(), window.height())
+                / (1.0f + 2.0f * player.vision_distance());
+    };
+    auto scale = auto_scale();
+
     for (bool quit = false; not quit;) {
-        SDL_Event event;
-        SDL_WaitEvent(&event);
+        SDL_Delay(10);
 
         auto const looking_at = player.position.centre();
+
         auto frame = renderer(5, 5, 5);
         frame.viewport.translate(-looking_at)
                 .reflect_y()
-                .scale(std::min(window.width(), window.height())
-                       / (1 + 2 * player.vision_distance()))
+                .scale(scale)
                 .translate({window.width() / 2.0f, window.height() / 2.0f});
 
-        planet::point2d towards{0, 0};
-        switch (event.type) {
-        case SDL_QUIT: quit = true; break;
-        case SDL_MOUSEBUTTONUP:
-            switch (event.button.button) {
-            case SDL_BUTTON_LEFT:
-                towards =
-                        frame.viewport.outof(
-                                {float(event.motion.x), float(event.motion.y)})
-                        - looking_at;
+        planet::point2d click_direction{0, 0};
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT: quit = true; break;
+            case SDL_MOUSEBUTTONUP:
+                switch (event.button.button) {
+                case SDL_BUTTON_LEFT:
+                    click_direction = frame.viewport.outof(
+                                              {float(event.motion.x),
+                                               float(event.motion.y)})
+                            - looking_at;
+                    break;
+                }
                 break;
             }
-            break;
         }
 
-        if (towards.mag2() > 2.0f) {
-            auto const theta = towards.theta();
+        if (click_direction.mag2() > 2.0f) {
+            auto const theta = click_direction.theta();
             auto const index = std::size_t(6.0f * (theta + 1.0f / 12.0f)) % 6;
             auto const outcome =
                     player.move(world, planet::hexmap::directions[index]);
 
             if (outcome.state != update::player::alive) {
-                std::cout << "You died. Your final score was "
-                          << player.current_score() << '\n';
+                std::cout << "You died. ";
                 quit = true;
             }
+        }
+
+        /// Next view location
+        auto target_scale = auto_scale();
+        if (target_scale > scale) {
+            scale = std::min(scale + 0.15f, target_scale);
+        } else if (target_scale < scale) {
+            scale = std::max(scale - 0.15f, target_scale);
         }
 
         draw::world(frame, world, player, player.vision_distance());
@@ -76,8 +93,10 @@ int main() {
         SDL_DestroyTexture(texture);
         SDL_FreeSurface(surface);
     }
+    std::cout << "Your final score was " << player.current_score() << '\n';
 
     TTF_CloseFont(font);
     TTF_Quit();
+
     return 0;
 }
