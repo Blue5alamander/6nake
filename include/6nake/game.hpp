3460bc7f03dc6204584a4a1911a8e34d0a4a568c
Kirit Sælensminde <kirit@kirit.com>
2022-11-03 09:41:52 +0700

    Start the game with a zoom animation

--- include/6nake/game.hpp
+++ include/6nake/game.hpp
@@ -41,7 +41,7 @@ namespace game {
         mapgen::hex::world_type world = mapgen::create_map();
         player::snake player = {world};
         planet::affine::point2d looking_at = player.position.centre();
-        float scale = calculate_auto_scale_factor();
+        float scale = calculate_auto_scale_factor() / 1000;
         planet::sdl::renderer renderer = {game.window};
         planet::sdl::drawframe arena = {renderer};
 
