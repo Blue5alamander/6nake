1a9f4ee59372ef5c58819f9f767fa0f6b28fec17
Kirit Sælensminde <kirit@felspar.com>
2023-12-27 19:57:42 +0700

    Remove unused argument

--- 6nake.cpp
+++ 6nake.cpp
@@ -6,7 +6,7 @@
 
 int main(int args, char const *argv[]) {
     return planet::sdl::co_main(
-            [](planet::sdl::init &sdl, int args,
+            [](planet::sdl::init &sdl, int,
                char const *argv[]) -> felspar::coro::task<int> {
                 game::main game{sdl, {argv[0]}};
                 co_return co_await game.run();
